6c49e2a145c3204f7d43efdea4e3ef300c70c672
Porfirio <porfirioribeiro@gmail.com>
2022-03-14 21:00:49 +0000

    add vue

---
+++ packages/vue/src/StaticMap.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { StaticMapsState, StaticMapOptions, createStaticMap } from '@staticmaps/core';
+
+const props = defineProps<{ options: StaticMapOptions }>();
+const o = createStaticMap(props.options);
+</script>
+
+<template>
+  <svg :width="o.width" :height="o.height" :viewBox="o.viewBox">
+    <image v-for="tile in o.tiles" v-bind="tile"></image>
+    <path v-for="multiPolygon in o.multiPolygons" v-bind="multiPolygon" vectorEffect="non-scaling-stroke"></path>
+  </svg>
+  <div>Hello</div>
+</template>
