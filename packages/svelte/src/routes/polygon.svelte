2fa5aa2f5c4467cc15aed1ca08af4bbc84837063
Porfirio <porfirioribeiro@gmail.com>
2022-04-09 17:19:09 +0100

    feat(demo): add polygons with holes examples

--- packages/svelte/src/routes/polygon.svelte
+++ packages/svelte/src/routes/polygon.svelte
@@ -1,35 +1,11 @@
 <script lang="ts">
 	import StaticMap from '$lib/StaticMap.svelte';
 	import { osmTileProvider, type MultiPolygon } from '@staticmaps/core';
-
-	export const multiPolygon: MultiPolygon = {
-		fill: 'transparent',
-		stroke: '#00C4B3CC',
-		strokeWidth: 2,
-		coords: [
-			// polygon
-			[
-				// first linear ring is outer coordinates
-				[
-					[-80.19, 25.774],
-					[-66.118, 18.466],
-					[-64.757, 32.321],
-					[-80.19, 25.774],
-				],
-				// other linear rings are holes
-				// todo: support holed polygons
-				// [
-				// 	[-70.579, 28.745],
-				// 	[-67.514, 29.57],
-				// 	[-66.668, 27.339],
-				// 	[-70.579, 28.745],
-				// ]
-			]
-		]
-	};
 </script>
 
-<h1>Polygons</h1>
+<h1 class="py-8">Polygons</h1>
+
+<h3 class="py-16 mt-24">Polygon with 2 holes</h3>
 
 <StaticMap
 	options={{
@@ -37,6 +13,80 @@
 		height: 500,
 		padding: [10, 10],
 		tileProvider: osmTileProvider,
-		multiPolygons: [multiPolygon]
+		multiPolygons: [
+			{
+				fill: 'red',
+				stroke: '#00C4B3CC',
+				strokeWidth: 2,
+				coords: [
+					[
+						[
+							[-80.19, 25.77],
+							[-66.11, 18.46],
+							[-64.75, 32.32],
+							[-80.19, 25.77]
+						],
+						[
+							[-76.37, 25.79],
+							[-71.19, 27.95],
+							[-67.11, 26.15],
+							[-67.41, 20.22],
+							[-76.37, 25.79]
+						],
+						[
+							[-70.57, 28.74],
+							[-67.51, 29.57],
+							[-66.66, 27.33],
+							[-70.57, 28.74]
+						]
+					]
+				]
+			}
+		]
+	}}
+/>
+
+<h3 class="py-16 mt-24">Multiple Polygons with and without hole</h3>
+
+<StaticMap
+	options={{
+		width: 500,
+		height: 500,
+		padding: [10, 10],
+		tileProvider: osmTileProvider,
+		multiPolygons: [
+			{
+				fill: 'blue',
+				fillOpacity: 0.2,
+				stroke: 'blue',
+				strokeWidth: 2,
+				coords: [
+					[
+						[
+							[40.0, 40.0],
+							[20.0, 45.0],
+							[45.0, 30.0],
+							[40.0, 40.0]
+						]
+					],
+					[
+						[
+							[20.0, 35.0],
+							[10.0, 30.0],
+							[10.0, 10.0],
+							[30.0, 5.0],
+							[45.0, 20.0],
+							[20.0, 35.0]
+						],
+						[
+							[30.0, 20.0],
+							[20.0, 15.0],
+							[20.0, 25.0],
+							[30.0, 20.0]
+						]
+					]
+				]
+			}
+		]
 	}}
 />
