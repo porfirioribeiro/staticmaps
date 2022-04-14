0c46c0a1d64cac03175b88ab90683fa71517a3b8
Porfirio <porfirioribeiro@gmail.com>
2022-04-14 22:05:05 +0100

    fix: Remove dead code polyFromBBox

--- packages/core/src/helpers.ts
+++ packages/core/src/helpers.ts
@@ -1,18 +1,2 @@
-import { BBox, Position } from './types';
-
-export function polyFromBBox(bbox: BBox): Position[][][] {
-  return [
-    [
-      [
-        [bbox[0], bbox[1]],
-        [bbox[2], bbox[1]],
-        [bbox[2], bbox[3]],
-        [bbox[0], bbox[3]],
-        [bbox[0], bbox[1]],
-      ],
-    ],
-  ];
-}
-
 export const dashed = (camel: string) => camel.replace(/[A-Z]/g, m => '-' + m.toLowerCase());
 export const dashedKeys = (o: any) => Object.fromEntries(Object.entries(o).map(([k, v]) => [dashed(k), v]));
